5b5d248ccbcbb2e0cf2d59999844f3ea94bab256
yaredgdb123 <yaredgirmadb123@gmail.com>
2023-07-28 04:59:57 +0300

    removed duplicate code

--- erpnext/stock/api/api.py
+++ erpnext/stock/api/api.py
@@ -24,34 +24,6 @@ def add_items_to_system():
 		frappe.db.commit()
 		return True
 
-@frappe.whitelist()
-def create_purchase_order():
-	with open ('/Users/yaredgd/frappe-bench/apps/erpnext/erpnext/stock/api/stock.csv') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		for row in spamreader:
-			purchase_order = frappe.new_doc("Purchase Order")
-			purchase_order.supplier =  row[2]
-			purchase_order.company = 'Eco Green'
-			purchase_order.currency = 'ETB'
-			purchase_order.schedule_date = '2018-02-01'
-			purchase_order.set_posting_time = 1
-			purchase_order.set_warehouse = 1
-			purchase_order.taxes_and_charges = 'VAT 15% on Purchase'
-			purchase_order.taxes = 'VAT 15% on Purchase'
-			purchase_order.items = [
-				{
-					"item_code": row[0],
-					"item_name": row[7],
-					"description": row[7],
-					"uom": "Nos",
-					"qty": row[4],
-					"rate": row[6],
-					"amount": row[6]
-				}
-			]
-			purchase_order.insert()
-		frappe.db.commit()
-		return True
 
 @frappe.whitelist()
 def update_item_pricing ():
